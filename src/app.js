e7718e032d50ebc2260c8520699fc85c80bb11d9
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-26 11:16:29 -0800

    Ajusts to be used with GoBarber-Web (frontend)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -25,6 +26,9 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    // cors usage example:
+    // this.server.use(cors({ origin: 'https://alloweddomain.com' }));
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
